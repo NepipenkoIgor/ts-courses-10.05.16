bf4d72c80960264589228fc06f85de8717896624
Nepipenkoigor <igornepipenko@gmail.com>
2016-05-20 18:38:12 +0300

    lesson-3

---
+++ lesson-3/scripts/flikr.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by igor on 1/13/16.
+ */
+// https://developer.mozilla.org/en-US/docs/Web/API/GlobalFetch/fetch
+// https://github.com/iliakan/ts-course/blob/master/ts-lesson-3/demo-flikr-app/scripts/fetch.ts
+// uri: 'https://api.flickr.com/services/rest/?',
+// apiKey: '7fbc4d0fd04492d32fa9a2f718c6293e'
+// queryMethod: 'flickr.photos.search',
+// ${this.uri}method=${this.qyeryMethod}&api_key=${this.apiKey}&text=${text}&page=1&format=json&nojsoncallback=1`
+
