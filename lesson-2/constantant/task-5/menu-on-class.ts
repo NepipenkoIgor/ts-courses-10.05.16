93457e632057d57e2676dda115dad0f62d108a56
constantant <constantant@mail.ru>
2016-05-21 14:52:20 +0300

    task 5

--- lesson-2/constantant/task-5/menu-on-class.ts
+++ lesson-2/constantant/task-5/menu-on-class.ts
@@ -2,48 +2,48 @@
  * Created by igor on 1/6/16.
  */
 
-type menuList = { title:string, id?:string, items?:menuList }[];
+type menuList = { title: string, id?: string, items?: menuList }[];
 
 interface MenuInterface {
-    getElem():HTMLElement;
-    toggle(id:string):void;
-    close(id:string):void;
-    open(id:string):void;
-    render(parent?:HTMLElement):void;
+    getElem(): HTMLElement;
+    toggle(id: string): void;
+    close(id: string): void;
+    open(id: string): void;
+    render(parent?: HTMLElement): void;
 }
 
 class Menu implements MenuInterface {
-    private element:HTMLElement;
-    private menuList:menuList;
-    private id:number;
-    static increment:number = 0;
+    private element: HTMLElement;
+    private menuList: menuList;
+    private id: number;
+    static increment: number = 0;
 
-    constructor(menuList:menuList) {
+    constructor(menuList: menuList) {
         this.menuList = menuList;
         this.id = Menu.increment++;
     }
 
-    public getElem():HTMLElement {
+    public getElem(): HTMLElement {
         return this.element;
     }
 
-    public toggle(id:string):void {
-        let elem:HTMLElement = this.getItemById(id);
+    public toggle(id: string): void {
+        let elem: HTMLElement = this.getItemById(id);
         elem.classList.toggle('menu-open');
     }
 
-    public close(id:string):void {
-        let elem:HTMLElement = this.getItemById(id);
+    public close(id: string): void {
+        let elem: HTMLElement = this.getItemById(id);
         elem.classList.remove('menu-open');
     }
 
-    public open(id:string):void {
-        let elem:HTMLElement = this.getItemById(id);
+    public open(id: string): void {
+        let elem: HTMLElement = this.getItemById(id);
         elem.classList.add('menu-open');
     }
 
-    public render(parent?:HTMLElement):void {
-        let parentElement:HTMLElement = parent || document.body;
+    public render(parent?: HTMLElement): void {
+        let parentElement: HTMLElement = parent || document.body;
 
         this.element = document.createElement('div');
         this.element.classList.add('menu');
@@ -53,7 +53,7 @@ class Menu implements MenuInterface {
         this.element.addEventListener('click', Menu.clickHandler);
     }
 
-    static clickHandler(e:MouseEvent):void {
+    static clickHandler(e: MouseEvent): void {
         let el = <HTMLElement>e.target;
         let classList = el.classList;
         if (classList.contains('title')) {
@@ -63,12 +63,17 @@ class Menu implements MenuInterface {
     }
 
     // document не есть хорошо . Может от елемента меню??
-    private getItemById(id:string):HTMLElement {
-        return document.getElementById(id + '-' + this.id);
+    // getElementById есть только у document https://learn.javascript.ru/searching-elements-dom, это самый быстрый способ, а в качестве контекста тут можно считать this.id
+    private getItemById(id: string): HTMLElement {
+        return document.getElementById(this.getDomId(id));
     }
 
-    private generateMenu(items:menuList):string {
-        let html:string = '';
+    private getDomId(id: string): string {
+        return id + '-' + this.id;
+    }
+
+    private generateMenu(items: menuList): string {
+        let html: string = '';
         if (items && !Array.isArray(items)) {
             throw new TypeError('variable "items" must be like an Array');
         }
@@ -80,12 +85,12 @@ class Menu implements MenuInterface {
         html += `<ul>`;
         for (let item of items) {
 
-            let id:string = item.id,
-                title:string = item.title,
-                items:menuList = item.items,
-                hasItems:boolean = !!(items && items.length);
+            let id: string = item.id,
+                title: string = item.title,
+                items: menuList = item.items,
+                hasItems: boolean = !!(items && items.length);
 
-            html += `<li${id ? ` id="${id}-${this.id}"` : ''}>
+            html += `<li${id ? ` id="${this.getDomId(id)}"` : ''}>
                 <a class="title${hasItems ? '' : '-leaf'}">${title}</a>
                 ${this.generateMenu(items)}
             </li>`;
@@ -96,7 +101,7 @@ class Menu implements MenuInterface {
     }
 }
 
-let menuList:menuList = [
+let menuList: menuList = [
     {
         title: 'Животные',
         id: 'animals',
