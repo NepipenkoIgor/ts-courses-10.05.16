bb7291e9ceb2411d71139dcd34fe2802f09a8b6f
constantant <constantant@mail.ru>
2016-05-21 12:16:27 +0300

    task 1

--- lesson-2/constantant/task-1/is-in-array.ts
+++ lesson-2/constantant/task-1/is-in-array.ts
@@ -2,42 +2,46 @@
  * Created by constantant on 18.05.2016.
  */
 
-function isInArray(array: any[], ...targets: any[]):boolean {
+function isInArray<T>(array: T[], ...targets: T[]): boolean {
 
-    if(!Array.isArray(array)){
+    if (!Array.isArray(array)) {
         throw new TypeError('First argument must be like an Array! ');
     }
 
-    if(targets.length === 0){
+    if (targets.length === 0) {
         throw new Error('No target!');
     }
 
     // работа в массиве с массивом не есть хорошо и почему не ()=>{}
-    return targets.every(function (target:any) {
+    /* return targets.every(function (target:any) {
         return array.some(function (item:any) {
             return item === target;
         });
+    }); */
+
+    return targets.every(( target: T ) => {
+        return array.indexOf(target) !== -1;
     });
 }
 
 
+type test1 = number | {a: number};
+type test2 = number | number[];
+type test34 = test2 | string;
 
-let arr:any[] = [],
-    num1:number = 1,
-    num2:string = '2',
-    obj1:{a:number} = {a:1},
-    obj2:number[] = [1,2,3];
+let arr: any[] = [],
+    num1: number = 1,
+    num2: string = '2',
+    obj1: {a: number} = {a: 1},
+    obj2: number[] = [1, 2, 3];
 
-arr.push(num1);
-arr.push(num2);
-arr.push(obj1);
-arr.push(obj2);
+arr.push(num1, num2, obj1, obj2);
 
 console.log(
-    isInArray(arr, num1, obj1),
-    isInArray(arr, 33, obj2),
-    isInArray(arr, obj2, num2, num1),
-    isInArray(arr, obj2, '1212', num1)
-    //isInArray(arr)
-    //isInArray(123, obj2, '1212', num1)
+    isInArray<test1>(arr, num1, obj1),
+    isInArray<test2>(arr, 33, obj2),
+    isInArray<test34>(arr, obj2, num2, num1),
+    isInArray<test34>(arr, obj2, '1212', num1)
+    // isInArray(arr)
+    // isInArray(123, obj2, '1212', num1)
 );
